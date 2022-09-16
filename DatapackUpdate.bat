f1e5625d17061d0e6bb1b9947d346883b00269b9
Kamiloso <kamiloso.skoposo@gmail.com>
2022-09-16 23:49:13 +0200

    Batch update 2

--- DatapackUpdate.bat
+++ DatapackUpdate.bat
@@ -2,7 +2,7 @@
 
 xcopy /y/v .\SourcePack.jse3 .\Assets\Game\Other\TextAssets\SourcePack.jse3.txt
 
-xcopy /y/v .\SourcePack.jse3 .\Ready\Datapacks\Default.jse3
+xcopy /y/v .\SourcePack.jse3 .\WindowsBuild\Datapacks\Default.jse3
 xcopy /y/v .\SourcePack.jse3 .\LinuxBuild\Datapacks\Default.jse3
 xcopy /y/v .\SourcePack.jse3 .\MacBuild\Datapacks\Default.jse3
 xcopy /y/v .\SourcePack.jse3 .\DevBuild\Datapacks\Default.jse3
