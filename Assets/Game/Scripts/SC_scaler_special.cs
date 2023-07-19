7699cb31a5cd005c240890c7b019e7daf44af4df
Kamil <kamiloso.skoposo@gmail.com>
2023-07-19 10:43:27 +0200

    2.0 Release Candidate 3

--- Assets/Game/Scripts/SC_scaler_special.cs
+++ Assets/Game/Scripts/SC_scaler_special.cs
@@ -5,22 +5,10 @@ using UnityEngine;
 public class SC_scaler_special : MonoBehaviour
 {
     public float light_range = 0f;
-    public bool get_particles_scale;
-    Vector3 scaler = new Vector3(1f,1f,1f);
-    bool prer = false;
+    public Vector3 scaler = new Vector3(1f,1f,1f);
 
-    void Pre()
-    {
-        if(get_particles_scale)
-            scaler = transform.localScale;
-    }
     public void ScaleNow(float percentage)
     {
-        if(!prer) {
-            Pre();
-            prer = true;
-        }
-
         Light light = transform.GetComponent<Light>();
         ParticleSystem particles = transform.GetComponent<ParticleSystem>();
         if(light!=null) light.range = light_range * percentage;
