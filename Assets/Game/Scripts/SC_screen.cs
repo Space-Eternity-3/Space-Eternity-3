a13e468caacef012580291644fcb1e1c96a27c3c
Kamil <kamiloso.skoposo@gmail.com>
2024-04-26 23:48:17 +0200

    Beta 2.2 (probably release)

--- Assets/Game/Scripts/SC_screen.cs
+++ Assets/Game/Scripts/SC_screen.cs
@@ -4,31 +4,54 @@ using UnityEngine;
 
 public class SC_screen : MonoBehaviour
 {
-    public static bool fullS;
     public static bool already_started = false;
+    public static bool fullS;
+
+    public static int defX;
+    public static int defY;
+
+    public static int reload_counter = 0;
 
     void Start()
     {
         if(!already_started)
         {
+            defX = Screen.currentResolution.width;
+            defY = Screen.currentResolution.height;
             fullS = true;
             already_started = true;
         }
+        ReloadScreen();
     }
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.F11)) fullS = !fullS;
-        if(fullS && !Screen.fullScreen) SetFull();
-        if(!fullS && Screen.fullScreen) SetWindowed();
+        if(Input.GetKeyDown(KeyCode.R)) ReloadScreen();
+
+        if(fullS) {
+            if(reload_counter==2) SetFull2();
+            if(reload_counter==1) SetFull();
+        }
+        else {
+            if(reload_counter==2) SetWindowed2();
+            if(reload_counter==1) SetWindowed();
+        }
+
+        if(reload_counter==0)
+        {
+            if(Input.GetKeyDown(KeyCode.F11)) fullS = !fullS;
+            if(fullS && !Screen.fullScreen) SetFull();
+            if(!fullS && Screen.fullScreen) SetWindowed();
+        }
+        else reload_counter--;
     }
-    void SetFull()
+
+    void SetFull()      { Screen.SetResolution(defX,defY,true); }
+    void SetWindowed()  { Screen.SetResolution(1280,720,false); }
+    void SetFull2()     { Screen.SetResolution(defX-1,defY-1,true); }
+    void SetWindowed2() { Screen.SetResolution(1280-1,720-1,false); }
+
+    void ReloadScreen()
     {
-        Screen.SetResolution(Screen.currentResolution.width,Screen.currentResolution.height,true);
-        Screen.fullScreen=true;
-    }
-    void SetWindowed()
-    {
-        Screen.SetResolution(1280,720,true);
-        Screen.fullScreen=false;
+        reload_counter = 2;
     }
 }
